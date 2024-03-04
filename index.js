38ba5367620e8e03245c81b6eda369cc9d31699c
Ahammad204 <Ahammadcode@gmail.com>
2024-03-04 22:47:48 +0600

    Initial Server setup

---
+++ index.js
@@ -0,0 +1,26 @@
+ //Initial index.js setup for server
+ const express = require('express');
+ const cors = require('cors');
+ require ('dotenv').config();
+ const app = express();
+
+ //Middleware
+ app.use(cors());
+ app.use(express.json());
+
+//port
+const port = process.env.PORT || 5000;
+
+//For knowing that server is working or not
+app.get("/", (req, res) => {
+
+  res.send("Server is Running....")
+
+});
+
+//For knowing which port we are use
+app.listen(port, () => {
+
+  console.log(`Server is running on port ${port}`);
+
+})
